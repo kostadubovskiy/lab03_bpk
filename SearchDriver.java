8852c4f5d11b16dc4554889c371681ecc5b0ea4d
PearMeow <phuang30@stuy.edu>
2021-12-17 20:19:06 -0500

    hi

---
+++ SearchDriver.java
@@ -0,0 +1,11 @@
+public class SearchDriver {
+
+
+
+    public static void main (String[] args) {
+    long startTime = System.currentTimeMillis(); //start time
+    // insert code here 
+    long endTime = System.currentTimeMillis();   //end time
+    System.out.println(endTime - startTime);     //elapsed time 
+}
+}
