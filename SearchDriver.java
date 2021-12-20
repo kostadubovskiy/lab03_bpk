ece7e1415b4e2908df2a8ba61805331d60444a0c
PearMeow <phuang30@stuy.edu>
2021-12-20 17:00:15 -0500

    hi

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,18 +1,19 @@
 public class SearchDriver {
 
     public static void main (String[] args) {
-    Comparable[] array = new Comparable[102000000];
+    int almostMax = 212000000; 
+    Comparable[] array = new Comparable[almostMax + 1];
     for (int i = 0; i < array.length; i++) {
       array[i] = (Comparable) i;
     }
 
     long startTime = System.currentTimeMillis(); //start time
-    BinSearch.binSearchRec((Comparable[])array, 21, 22, 0);
+    BinSearch.binSearchRec(array, almostMax, 0, almostMax);
     long endTime = System.currentTimeMillis();   //end time
     System.out.println(endTime - startTime);     //elapsed time
 
     startTime = System.currentTimeMillis(); //start time
-    LinSearch.linSearch((Comparable[])array, 21);
+    LinSearch.linSearch(array, almostMax);
     endTime = System.currentTimeMillis();   //end time
     System.out.println(endTime - startTime);     //elapsed time
 
