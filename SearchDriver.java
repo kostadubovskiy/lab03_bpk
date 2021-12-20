1119d6d24d076f029cfd305572a12c772c070700
phuang30 <phuang30@stuy.edu>
2021-12-20 13:19:23 -0500

    hi

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,12 +1,21 @@
-import java.util.ArrayList;
-
 public class SearchDriver {
 
-
     public static void main (String[] args) {
+    Comparable[] array = new Comparable[102000000];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = (Comparable) i;
+    }
+
     long startTime = System.currentTimeMillis(); //start time
-    // insert code here 
+    BinSearch.binSearchRec((Comparable[])array, 21, 22, 0);
     long endTime = System.currentTimeMillis();   //end time
-    System.out.println(endTime - startTime);     //elapsed time 
+    System.out.println(endTime - startTime);     //elapsed time
+
+    startTime = System.currentTimeMillis(); //start time
+    LinSearch.linSearch((Comparable[])array, 21);
+    endTime = System.currentTimeMillis();   //end time
+    System.out.println(endTime - startTime);     //elapsed time
+
+
 }
 }
